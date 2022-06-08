5020fee230ab981c8c6f6ecd347a810a643d99ed
Abhay Singh <abhay9769.as@gmail.com>
2022-06-08 21:44:19 +0530

    Updated the Styles

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 api_key = "f82efcc559a1b6e4f0d3c82842903f451aec8bde"
 og_url = ""
 
-
+def checkurl():
+    if len(og_url)>32:
+        return og_url[:32]
 
 def shorturl():
     inpu_api = pyshorteners.Shortener(api_key=api_key)
@@ -30,12 +32,13 @@ def result():
     #     return render_template('click.html', urlshort=urlshort)
     og_url = request.form['original_url']
     urlshort, inpu_api = shorturl()
+    og_url=checkurl()
     return render_template('result.html', urlshort=urlshort, og_url=og_url)
 
 @app.route('/click', methods=['GET', 'POST'])
 def click():
     no_of_clicks = clicks()
-    return render_template('click.html', no_of_clicks=no_of_clicks)
+    return render_template('click.html', no_of_clicks=no_of_clicks, og_url=og_url)
 
 # print(urlshort)
 
